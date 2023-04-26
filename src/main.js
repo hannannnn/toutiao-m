a935214d103a44b998468079be2f4e0eaf387238
han <1477994353@qq.com>
2023-04-26 13:05:34 +0800

    项目初始化-引入vant组件库’
    git commit -m 项目初始化-引入vant组件库’

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入Vant组件库
+import Vant from 'vant'
+// 引入vant组件库全局样式
+import 'vant/lib/index.css'
+
 import './styles/index.less'
+// 注册使用Vant组件库
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
